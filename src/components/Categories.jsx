b6664b9413bcaaf71e24b9caa9c51bbbb3e80857
Andrey Chebanov <pioneerbeat@yandex.ru>
2022-12-22 22:36:51 +0300

    add/some components

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Categories = () => {
+  const categoriesArr = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const [active, setActive] = React.useState(0);
+
+  const setActiveFunc = (index) => {
+    setActive(index);
+  };
+
   return (
     <div className="categories">
-    <ul>
-      <li class="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-    </ul>
-  </div>
-  )
-}
+      <ul>
+        {categoriesArr.map((item, index) => (
+          <li
+            key={index}
+            className={active === index ? "active" : ""}
+            onClick={() => setActiveFunc(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
